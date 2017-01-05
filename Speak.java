0b7aa1ee08ba0995931a98711d457bac9a82388d
mahendra-16 <prajapatimahendra@ymail.com>
2017-01-05 18:44:42 +0530

    Spelling

---
+++ Speak.java
@@ -0,0 +1,15 @@
+package com.kontainer;
+
+public class Speak {
+	private String name = "Jeetendra Mishra";
+
+	public void getName() {
+		System.out.println("Hello : " + name);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
